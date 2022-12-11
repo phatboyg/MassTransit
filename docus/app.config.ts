cb6108fa9e4bc609b8b5c4c0e6dca099b4b279ae
Chris Patterson <chris@phatboyg.com>
2022-12-11 08:12:54 -0600

    Initial branch for documentation/website rebuild

---
+++ docus/app.config.ts
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  github: {
+    owner: 'MassTransit',
+    repo: 'MassTransit',
+    branch: 'develop'
+  },
+  docus: {
+    title: 'MassTransit',
+    description: 'An open-source distributed application framework for .NET',
+    url: 'https://masstransit-project.com',
+    socials: {
+      twitter: 'mtproj',
+      github: 'MassTransit/MassTransit'
+    },
+    aside: {
+      level: 1,
+      exclude: []
+    },
+    header: {
+      showLinkIcon: true,
+      exclude: []
+    }
+  }
+})
