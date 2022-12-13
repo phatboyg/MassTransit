a030da64bd8942c12467503c4aeae4a0f463c1fc
Chris Patterson <chris@phatboyg.com>
2022-12-12 19:28:05 -0600

    Updated formatting for syntax highlighting

--- docus/nuxt.config.ts
+++ docus/nuxt.config.ts
@@ -1,6 +1,19 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus',
-  colorMode: {
-    preference: 'dark'
-  }
+    extends: '@nuxt-themes/docus',
+    colorMode: {
+        preference: 'dark'
+    },
+    content: {
+        documentDriven: true,
+        highlight: {
+            theme: {
+                dark: 'material-darker',
+                default: 'material-lighter'
+            },
+            preload: ['json', 'shell', 'markdown', 'yaml', 'bash', 'csharp']
+        },
+        navigation: {
+            fields: ['icon', 'titleTemplate', 'aside']
+        }
+    }
 })
