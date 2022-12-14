d2613442dcfffbf668459c8ec01798ff04c2755e
Chris Patterson <chris@phatboyg.com>
2022-12-13 22:25:55 -0600

    Added component to list items in configuration

---
+++ docus/components/global/FolderNavigation.vue
@@ -0,0 +1,24 @@
+<script setup>
+const navigationTree = await queryContent('documentation', 'configuration').find();
+</script>
+
+<template>
+  <ul class="navigation">
+    <li v-for="(item, index) in navigationTree" :key="index">
+      <NuxtLink :to="item._path">
+        {{ item.title }}
+      </NuxtLink>
+      <FolderNavigation v-if="item.children" :navigation-tree="item.children" class="sub-navigation" />
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.navigation li {
+  list-style-type: '- ';
+}
+
+.navigation ul {
+  margin-left: .8rem;
+}
+</style>
