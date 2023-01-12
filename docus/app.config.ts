cc7da46a4709ebdc6261b52e0a60a2f0910a59cd
Chris Patterson <chris@phatboyg.com>
2023-01-12 11:51:01 -0600

    Fixed meta image

--- docus/app.config.ts
+++ docus/app.config.ts
@@ -1,24 +1,35 @@
 export default defineAppConfig({
-  github: {
-    owner: 'MassTransit',
-    repo: 'MassTransit',
-    branch: 'develop'
-  },
-  docus: {
-    title: 'MassTransit',
-    description: 'An open-source distributed application framework for .NET',
-    url: 'https://masstransit-project.com',
-    socials: {
-      twitter: 'mtproj',
-      github: 'MassTransit/MassTransit'
+    github: {
+        owner: 'MassTransit',
+        repo: 'MassTransit',
+        branch: 'develop'
     },
-    aside: {
-      level: 1,
-      exclude: []
-    },
-    header: {
-      showLinkIcon: true,
-      exclude: []
+    docus: {
+        title: 'MassTransit',
+        description: 'An open-source distributed application framework for .NET',
+        url: 'https://masstransit-project.com',
+        socials: {
+            twitter: 'mtproj',
+            github: 'MassTransit/MassTransit'
+        },
+        aside: {
+            level: 1,
+            exclude: []
+        },
+        cover: {
+            src: 'https://raw.githubusercontent.com/phatboyg/MassTransit/docus/docus/public/mt-logo-color.png',
+            alt: 'MassTransit Logo',
+        },
+        header: {
+            showLinkIcon: true,
+            exclude: []
+        },
+        footer: {
+            credits: {
+                text: 'Copyright 2023 Chris Patterson',
+                href: 'https://masstransit.io',
+            },
+            icons: [],
+        },
     }
-  }
 })
